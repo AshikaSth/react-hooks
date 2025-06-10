4e09b49d3babf5ac4818b730c108cddc7b048ded
Ashika <ashikashrestha114@gmail.com>
2025-06-10 07:00:17 +0545

    final useState

--- src/components/HookCounter4.js
+++ src/components/HookCounter4.js
@@ -8,6 +8,12 @@ function HookCounter4() {
       id: items.length,
       value: Math.floor(Math.random() * 10) + 1
     }])
+    //we first take the current items array, which is an empty array at the start, and then we add a new object to it.
+    //The new object has an id which is the current length of the items array, and a value which is a random number between 1 and 10. 
+    //firstsly, we use the spread operator to take all the current items in the array and then we add a new object to it.
+    //items.length is 0 at the start, so the first item will have an id of 0.
+    //The Math.random() function generates a random number between 0 and 1, and we multiply it by 10 to get a number between 0 and 10.
+    //This will create a new array with the current items and the new item, and then set the items state to that new array.
   } 
   return (
     <div>
